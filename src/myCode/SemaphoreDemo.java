d76c1058860d6833aab518b92684dc4de801ef49
满浩龙 <manhaolong@126.com>
2018-03-18 23:28:07 +0800

    共享资源的互斥使用(抢车位)

---
+++ src/myCode/SemaphoreDemo.java
@@ -0,0 +1,44 @@
+package myCode;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ * @author zzyy
+ * @date 2018年3月17日
+ * 
+ * 在信号量上我们定义两种操作：
+ * acquire（获取） 当一个线程调用acquire操作时，它要么通过成功获取信号量（信号量减1），
+ * 					要么一直等下去，直到有线程释放信号量，或超时。
+ * release（释放）实际上会将信号量的值加1，然后唤醒等待的线程。
+ * 
+ * 信号量主要用于两个目的，一个是用于多个共享资源的互斥使用，另一个用于并发线程数的控制。
+ */
+public class SemaphoreDemo
+{
+	public static void main(String[] args)
+	{
+		Semaphore semaphore = new Semaphore(3);//模拟3个停车位
+		
+		for (int i = 1; i <=6; i++) //模拟6部汽车
+		{
+			new Thread(() -> {
+				try 
+				{
+					semaphore.acquire();
+					System.out.println(Thread.currentThread().getName()+"\t ***抢占了车位");
+					TimeUnit.SECONDS.sleep(new Random().nextInt(8));
+					System.out.println(Thread.currentThread().getName()+"\t 离开了车位");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}finally {
+					semaphore.release();
+				}
+			},String.valueOf(i)).start();
+		}
+		
+	}
+}
